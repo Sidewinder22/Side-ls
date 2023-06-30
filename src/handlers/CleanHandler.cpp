12849cdf1d50e476cd8b46d06098f0b27113a9f3
Sidewinder22 <sidewinder22@protonmail.com>
2023-06-30 22:11:02 +0200

    Improve CleanHandler

--- src/handlers/CleanHandler.cpp
+++ src/handlers/CleanHandler.cpp
@@ -4,9 +4,11 @@
 std::vector<std::string> CleanHandler::execute(std::vector<std::string> output)
 {
     std::vector<std::string> result;
+    result.reserve(output.size());
 
     std::ranges::transform(output, std::back_inserter(result),
-        [](std::string entry){ return entry.substr(2); });
+        [](const std::string& entry){
+                return entry.substr(entry.find_last_of('/') + 1); });
 
     return result;
 }
--- src/handlers/LongListingHandler.cpp
+++ src/handlers/LongListingHandler.cpp
@@ -1,10 +1,10 @@
 #include <algorithm>
 #include "LongListingHandler.hpp"
 
-std::vector<std::string> LongListingHandler::execute(
-    std::vector<std::string> output)
+std::vector<std::string> LongListingHandler::execute(std::vector<std::string> output)
 {
     std::vector<std::string> result;
+    result.reserve(output.size());
 
     std::ranges::transform(output, std::back_inserter(result),
         [](std::string entry){ return entry + "\n"; });
